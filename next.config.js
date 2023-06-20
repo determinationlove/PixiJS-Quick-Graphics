5fe9e297b3adabe0925c8820bf9e5ff50d3e2981
lehcaR <47052855+determinationlove@users.noreply.github.com>
2023-06-21 00:21:01 +0800

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: '/PixiJS-Quick-Graphics',
+    basePath: '/pixiJS-Quick-Graphics',
 };
 
 module.exports = nextConfig;
